4b5aab8c7e038bc29d974be9de7b9219f5f66c94
MythNPC <wrekk.game@gmail.com>
2019-09-09 22:44:58 +1000

    Updates

---
+++ precache-manifest.5d0be1d7d651ffda58ea21a6254a00d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9348a17d2047c6d6aa3fcc74cbb36f3c",
+    "url": "/AppTestReact/index.html"
+  },
+  {
+    "revision": "7d51e9c2f8d1d0794ad4",
+    "url": "/AppTestReact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f0ce84b374f6e42a6546",
+    "url": "/AppTestReact/static/js/2.468b2f5f.chunk.js"
+  },
+  {
+    "revision": "7d51e9c2f8d1d0794ad4",
+    "url": "/AppTestReact/static/js/main.dbb76712.chunk.js"
+  },
+  {
+    "revision": "bded8cc5bad930910ac5",
+    "url": "/AppTestReact/static/js/runtime~main.116f218e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AppTestReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
